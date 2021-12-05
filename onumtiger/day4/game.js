3ec065ae82be73b6f089316703219ce40026d496
Daniel Onumbu <onum@Daniels-MBP.fritz.box>
2021-12-05 12:49:10 +0100

    day 4 part 1

---
+++ onumtiger/day4/game.js
@@ -0,0 +1,99 @@
+import { bingoSequence, bingoBoards } from "./bingoSequence.js";
+let testArray = [
+  [[67, "marked"], 7, 75, 66, 4],
+  [35, 97, 21, 29, 95],
+  [58, 98, 56, 71, 65],
+  [55, 61, 19, 64, 9],
+  [38, 34, 42, 30, 2],
+];
+
+const checkRowIsMarked = (inputRow) => {
+  let isRowMarked = false;
+  for (let i = 0; i < inputRow.length; i++) {
+    if (inputRow[i].length) isRowMarked = true;
+    else isRowMarked = false;
+    if (!isRowMarked) {
+      return isRowMarked;
+    }
+  }
+  return isRowMarked;
+};
+
+const checkColumnIsMarked = (inputArray) => {
+  let column = 0;
+  let row = 0;
+  let marked = 0;
+
+  while (column < inputArray.length) {
+    while (row < inputArray.length) {
+      if (inputArray[row][column].length) marked++;
+      row++;
+    }
+    if (marked == inputArray.length) {
+      return true;
+    }
+    row = 0;
+    marked = 0;
+    column++;
+  }
+  return false;
+};
+
+const calculateBoardSum = (inputArray) => {
+  let boardSum = 0;
+  for (let i = 0; i < inputArray.length; i++) {
+    for (let j = 0; j < inputArray.length; j++) {
+      if (!inputArray[i][j].length) {
+        boardSum = boardSum + inputArray[i][j];
+      }
+    }
+  }
+  return boardSum;
+};
+
+// console.log(calculateBoardSum(testArray));
+
+let winningBoard = [[]];
+let winningDraw = -1;
+for (let draw = 0; draw < bingoSequence.length; draw++) {
+  const currentDraw = bingoSequence[draw];
+  let bingo = false;
+  let bingoInRow = false;
+  let bingoInColumn = false;
+  for (let bingoBoard = 0; bingoBoard < bingoBoards.length; bingoBoard++) {
+    let currentBoard = bingoBoards[bingoBoard];
+    console.log(bingoBoard);
+    console.log(draw);
+    for (let row = 0; row < currentBoard.length; row++) {
+      currentBoard[row].forEach(function (entry, i) {
+        if (entry == currentDraw)
+          currentBoard[row][i] = [currentDraw, "marked"];
+      });
+    }
+    for (let row = 0; row < currentBoard.length; row++) {
+      if (checkRowIsMarked(currentBoard[row])) {
+        bingo = true;
+        bingoInRow = true;
+        break;
+      }
+    }
+    if (checkColumnIsMarked(currentBoard)) {
+      bingo = true;
+      bingoInColumn = true;
+    }
+
+    if (bingo) {
+      winningBoard = currentBoard;
+      winningDraw = currentDraw;
+      break;
+    }
+  }
+  if (bingo) {
+    break;
+  }
+}
+
+const boardSum = calculateBoardSum(winningBoard);
+const finalScore = boardSum * winningDraw;
+
+console.log(finalScore);
